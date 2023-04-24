d98b0a12769e4a972240befa5e40ab87fcd95291
Yann <ybarlet@gmail.com>
2023-04-24 15:12:25 +0200

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,32 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`);
+
+    try {
+        const resp = await fetch(currentURL);
+        if(resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType}, on page ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text());
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+
+    
+    
+    
+}
+
+
 /**
  * Parses an HTML body and returns an array of URLs found in the anchor (a) tags.
  * 
@@ -56,6 +83,7 @@ function normalizeURL(urlString) {
 module.exports = {
     normalizeURL,
     getURLsFromHTML,
+    crawlPage
 }
 
 // Alternatively, for ES6 module syntax:
--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("Hello world")
+const { crawlPage } = require('./crawl.js');
+
+function main () {
+    if (process.argv.length < 3) {
+        console.log('no website provided')
+        process.exit(1)
+    }
+    if (process.argv.length > 3) {
+        console.log('to many command line args')
+        process.exit(1)
+    }
+
+    // Can access the structure of process.arg array
+    // first and second element are not command line arg
+    // for(const arg of process.argv) {
+    //     console.log(arg)
+    // }
+
+    const baseURL = process.argv[2];
+
+    console.log(`starting crawl of ${baseURL}`)
+    crawlPage(baseURL);
+
+}
+
+main()
