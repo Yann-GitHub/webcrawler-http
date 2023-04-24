045951a636a8d4460b500688067be6b88b536755
Yann <ybarlet@gmail.com>
2023-04-24 10:54:16 +0200

    getURLsFromHTML and tests

--- README.md
+++ README.md
@@ -11,6 +11,30 @@ Once Node.js is installed, follow these steps:
 2. Open the terminal or command prompt and navigate to the directory where you extracted the files.
 3. Run npm install to install the required dependencies.
 
+### .nvmrc File
+
+A file named .nvmrc has been added to the project to specify the version of Node.js that should be used to run the application. This is useful because different projects may require different versions of Node.js, and having an .nvmrc file ensures that everyone working on the project is using the same version.
+
+The contents of the .nvmrc file specify the desired version of Node.js, for example:
+
+```bash
+18.15.0
+```
+To use the specified version of Node.js, you can install nvm (Node Version Manager) and run the following command in the project directory:
+
+```bash
+nvm use
+```
+This will use the version of Node.js specified in the .nvmrc file. If that version is not installed, nvm will prompt you to install it.
+
+Note that if you don't have nvm installed, you can still use the application with the version of Node.js you have installed on your computer. However, using the specified version in the .nvmrc file is recommended for consistency and to avoid potential issues caused by version differences.
+
+### JSDOM library
+
+The webcrawler-http application uses the JSDOM library to parse HTML and manipulate the resulting DOM. JSDOM is a JavaScript implementation of the W3C DOM standards that runs in Node.js. It allows you to write JavaScript code that manipulates HTML documents as if they were being rendered in a web browser.
+
+If you're not familiar with JSDOM, you can find more information in the [official documentation](https://github.com/jsdom/jsdom).
+
 ## Usage
 To run the `webcrawler-http` application, open the terminal or command prompt and navigate to the directory where you extracted the files. Then, run the following command:
 
--- crawl.js
+++ crawl.js
@@ -1,10 +1,47 @@
+const { JSDOM } = require('jsdom');
+
+/**
+ * Parses an HTML body and returns an array of URLs found in the anchor (a) tags.
+ * 
+ * @param {string} htmlBody - The HTML body containing the links.
+ * @param {string} baseURL - The base URL to use for resolving relative URLs.
+ * @returns {string[]} An array of URLs.
+ */
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements) {
+        if(linkElement.href.slice(0, 1) === '/') {
+            // relative url
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`);
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`);
+            }
+            
+        } else {
+            // absolute url
+            try {
+                const urlObj = new URL(linkElement.href);
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with absolute url: ${err.message}`);
+            }
+        }
+        
+    }
+    return urls;
+}
+
+
 /**
  * Normalizes a URL by converting different variations of the same URL to a standard format.
  * 
  * @param {string} urlString - The input URL string to be normalized
- * @returns {string} - The normalized URL string.
+ * @returns {string} The normalized URL string.
  */
-
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -15,7 +52,11 @@ function normalizeURL(urlString) {
     return hostPath
 }
 
-module.exports = { normalizeURL }
+// CommonJS syntaxe for import/export modules
+module.exports = {
+    normalizeURL,
+    getURLsFromHTML,
+}
 
 // Alternatively, for ES6 module syntax:
 // export default { normalizeURL }
